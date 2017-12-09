bcf208e8df454a81b487afa4e56f06b2e9ac5f38
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-12-09 21:20:26 +0200

    split components

--- src/Game.js
+++ src/Game.js
@@ -1,49 +1,5 @@
 import React from "react";
-
-function Square(props) {
-    return (
-        <button
-            className={props.winningSquares}
-            onClick={props.onClick}
-        >
-            {props.value}
-        </button>
-    );
-}
-
-class Board extends React.Component {
-    renderSquare(i, squares) {
-        return (
-            <Square
-                winningSquares={
-                    squares.filter((j) => j === i).length ?
-                        "square square--win" :
-                        "square"
-                }
-                key={i}
-                value={this.props.squares[i]}
-                onClick={() => this.props.onClick(i)}
-            />
-        );
-    }
-
-    render() {
-        let si = -1;
-
-        return <div>
-            {
-                Array(3).fill(null).map((a, i) =>
-                    <div key={i} className="board-row">
-                        {Array(3).fill(null).map(() => {
-                            si++;
-                            return this.renderSquare(si, this.props.winningSquares);
-                        })}
-                    </div>
-                )
-            }
-        </div>;
-    }
-}
+import Board from "./Board.js"
 
 export default class Game extends React.Component {
     constructor() {
